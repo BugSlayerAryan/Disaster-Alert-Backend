fc2b9b5aa87a08b4eb34a829b567d6de03133b70
Aryan <aryanbehera432@gmial.com>
2026-01-31 11:25:32 +0530

    MethodSecurityConfig add

---
+++ src/main/java/com/DisasterAlert/security/MethodSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.DisasterAlert.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity
+public class MethodSecurityConfig {
+    // This enables @PreAuthorize annotations in controllers
+}
