78a9760ea809026c10014390f55cd0a81735fbb4
Aryan <aryanbehera432@gmial.com>
2026-01-31 10:24:24 +0530

    Fix DB config for Render deployment

--- src/main/java/com/DisasterAlert/model/Admin.java
+++ src/main/java/com/DisasterAlert/model/Admin.java
@@ -1,5 +1,4 @@
 package com.DisasterAlert.model;
-
 import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
--- src/main/java/com/DisasterAlert/repository/AdminRepository.java
+++ src/main/java/com/DisasterAlert/repository/AdminRepository.java
@@ -1,5 +1,7 @@
 package com.DisasterAlert.repository;
 
+
+
 import com.DisasterAlert.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
